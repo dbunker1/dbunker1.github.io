caff284b151d3913c79ddb4bc8150e25cbf1502f
dj bunker <djbunker96@gmail.com>
2021-03-19 19:36:30 -0400

    alsdkjf

--- Path_to_Partner/ham_ScriptOne.js
+++ Path_to_Partner/ham_ScriptOne.js
@@ -1,271 +1,272 @@
-(function () {
-  // define variables
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var player = {};
-  var ground = [];
-  var platformWidth = 32;
-  var platformHeight = canvas.height - platformWidth * 4;
+var INTERVAL = 50;
+var DEBUG = false; //true;
 
-  /**
-   * Asset pre-loader object. Loads all images
-   */
-  var assetLoader = (function() {
-    // images dictionary
-    this.imgs        = {
-      'bg'            : 'bkg.png',
-     // 'sky'           : 'sky.png',
-     // 'backdrop'      : 'backdrop.png',
-     // 'backdrop2'     : 'backdrop_ground.png',
-     // 'grass'         : 'grass.png',
-      'avatar_normal' : 'sprite_0.png'
-    };
+var canvas;             // The canvas shown on the page.
+var ctx;                // The context, used to access the canvas.
 
-    var assetsLoaded = 0;                                // how many assets have been loaded
-    var numImgs      = Object.keys(this.imgs).length;    // total number of image assets
-    this.totalAssest = numImgs;                          // total number of assets
+var SpriteRow = 0;      // Row of the graphic to show
+var SpriteCol = 0;      // Col of the graphic to show
+var MaxSpriteRow = 8;   // How many rows of images
+var MaxSpriteCol = 8;  // How many columns of images
+var tileWidth = 32;    // Size of each tile (32x32)
+var tileHeight = 32;
+var goal_found = false;
+var goal_count = 0
+var GOALTILE = 5;      // Which tile is the goal
 
-    /**
-     * Ensure all assets are loaded before using them
-     * @param {number} dic  - Dictionary name ('imgs', 'sounds', 'fonts')
-     * @param {number} name - Asset name in the dictionary
-     */
-    function assetLoaded(dic, name) {
-      // don't count assets that have already loaded
-      if (this[dic][name].status !== 'loading') {
-        return;
-      }
 
-      this[dic][name].status = 'loaded';
-      assetsLoaded++;
+// Start the sprite on an even boundary (divisible by 32)
+var SpriteX = 96;      // Position of sprite on the canvas
+var SpriteY = 96;
+var SpriteWidth = 25;   // Width, Height of each subimage
+var SpriteHeight = 25;
 
-      // finished callback
-      if (assetsLoaded === this.totalAssest && typeof this.finished === 'function') {
-        this.finished();
-      }
-    }
+var WIDTH = 850;    // of the canvas
+var HEIGHT = 640;    // of the canvas
+var MAXROW = 20;
+var MAXCOL = 26;
+var moved = false; // Did the user move the square?
+var myX = 100;  // Where the square should go on the canvas
+var myY = 100;  // Where the square should go on the canvas
+var moveAmount = 8;  // How many pixels to move per update.
+var squareWidth = 10;   // Width, Height of the square
+var squareHeight = 10;
 
-    /**
-     * Create assets, set callback for asset loading, set asset source
-     */
-    this.downloadAll = function() {
-      var _this = this;
-      var src;
+var SpriteImage = new Image();   // Sprite sheet
+SpriteImage.src   = "sprite_0.png";
 
-      // load images
-      for (var img in this.imgs) {
-        if (this.imgs.hasOwnProperty(img)) {
-          src = this.imgs[img];
+var blueImage = new Image();   // Sprite sheet
+blueImage.src   = "bluesquare.png";
 
-          // create a closure for event binding
-          (function(_this, img) {
-            _this.imgs[img] = new Image();
-            _this.imgs[img].status = 'loading';
-            _this.imgs[img].name = img;
-            _this.imgs[img].onload = function() { assetLoaded.call(_this, 'imgs', img) };
-            _this.imgs[img].src = src;
-          })(_this, img);
-        }
-      }
-    }
+var featureImage = new Image();   // feature sheet
+featureImage.src   = "features.png";
 
-    return {
-      imgs: this.imgs,
-      totalAssest: this.totalAssest,
-      downloadAll: this.downloadAll
-    };
-  })();
 
-  assetLoader.finished = function() {
-    startGame();
+// Set up a timer to execute every 50 ms.
+// Begin code from 
+//   http://www.w3schools.com/jsref/met_win_setinterval.asp
+var myInterval;
+// End code from www.w3schools.com
+
+// A wall is a feature code of 1 
+// (i.e. 1 place right of left-most feature image)
+// Make this array 15 rows by 20 columns 
+// (each square is 32x32, so 15*32, 20*32 = 480, 640
+// 
+// There are other ways to do this. I like this way because
+// I can see where the features are in relation to each other.
+// Each value corresponds to a subimage of "features.png"
+// blank, wall, tree, ice, boulder, goal.
+var myarray = [ 
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+  1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
+  1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 
+  1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 
+  1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 1, 5, 0, 0, 1, 0, 1, 0, 0, 0, 1, 
+  1, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 3, 3, 3, 3, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 
+  1, 0, 1, 0, 0, 2, 2, 2, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 
+  1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 
+  1, 5, 1, 1, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 
+  1, 0, 1, 0, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 4, 0, 0, 1, 0, 1, 0, 1, 1, 1, 
+  1, 0, 1, 0, 1, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 
+  1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 4, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 
+  1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 5, 1, 0, 1, 
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 5, 1, 
+  1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 
+  1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 
+  1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 
+  1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 
+  1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,];
+
+
+
+// 
+// Change the background screen color to green.
+//
+function green() {
+  switch(goal_count)
+  {
+    case 0: 
+      var q1 = prompt("Do you choose this is that?"); 
+      console.log(q1);
+      break;
+    case 1: 
+     var q2 = prompt("Stay in bed or go to work?");
+     console.log(q2);
+      break;
+    case 3:
+      var q3 = prompt("Snitch or not?")
+      console.log(q3);
+      break;
+    case 4: 
+     var q4 =  prompt("What have you accomplished?")
+     console.log(q4);
+      window.location.href = "my_game_lvl2.html";
+      break;
   }
+  goal_count++; 
+  
+  
+}
 
-  /**
-   * Creates a Spritesheet
-   * @param {string} - Path to the image.
-   * @param {number} - Width (in px) of each frame.
-   * @param {number} - Height (in px) of each frame.
-   */
-  function SpriteSheet(path, frameWidth, frameHeight) {
-    this.image = new Image();
-    this.frameWidth = frameWidth;
-    this.frameHeight = frameHeight;
 
-    // calculate the number of frames in a row after the image loads
-    var self = this;
-    this.image.onload = function() {
-      self.framesPerRow = Math.floor(self.image.width / self.frameWidth);
-    };
+function eraseSprite() {
+  // erase sprite
+  ctx.clearRect(myX, myY, 32, 32);
+}
 
-    this.image.src = path;
-  }
 
-  /**
-   * Creates an animation from a spritesheet.
-   * @param {SpriteSheet} - The spritesheet used to create the animation.
-   * @param {number}      - Number of frames to wait for before transitioning the animation.
-   * @param {array}       - Range or sequence of frame numbers for the animation.
-   * @param {boolean}     - Repeat the animation once completed.
-   */
-  function Animation(spritesheet, frameSpeed, startFrame, endFrame) {
+// Look at each tile location. Draw the tile.
+function drawTiles() {
+  var index = 0;
+  var str;
+  for (var r=0; r<MAXROW; r++) {
+    // str = '';
+    for (var c=0; c<MAXCOL; c++) {
+      tile = myarray[index]; // [r* MAXCOL + c];
+      // str += tile + ' ';
+      ctx.drawImage(featureImage, 
+         // What part of the image (0 because there is only one row of images)
+         tile*tileWidth, 0, tileWidth, tileHeight, 
+         // Where it goes on the canvas
+         c*tileWidth, r*tileHeight, tileWidth, tileHeight); 
 
-    var animationSequence = [];  // array holding the order of the animation
-    var currentFrame = 0;        // the current frame to draw
-    var counter = 0;             // keep track of frame rate
-
-    // start and end range for frames
-    for (var frameNumber = startFrame; frameNumber <= endFrame; frameNumber++)
-      animationSequence.push(frameNumber);
-
-    /**
-     * Update the animation
-     */
-    this.update = function() {
-
-      // update to the next frame if it is time
-      if (counter == (frameSpeed - 1))
-        currentFrame = (currentFrame + 1) % animationSequence.length;
-
-      // update the counter
-      counter = (counter + 1) % frameSpeed;
-    };
-
-    /**
-     * Draw the current frame
-     * @param {integer} x - X position to draw
-     * @param {integer} y - Y position to draw
-     */
-    this.draw = function(x, y) {
-      // get the row and col of the frame
-      var row = Math.floor(animationSequence[currentFrame] / spritesheet.framesPerRow);
-      var col = Math.floor(animationSequence[currentFrame] % spritesheet.framesPerRow);
-
-      ctx.drawImage(
-        spritesheet.image,
-        col * spritesheet.frameWidth, row * spritesheet.frameHeight,
-        spritesheet.frameWidth, spritesheet.frameHeight,
-        x, y,
-        spritesheet.frameWidth, spritesheet.frameHeight);
-    };
-  }
-
-  /**
-   * Create a parallax background
-   */
-  var background = (function() {
-    var sky   = {};
-    var backdrop = {};
-    var backdrop2 = {};
-
-    /**
-     * Draw the backgrounds to the screen at different speeds
-     */
-    this.draw = function() {
-      ctx.drawImage(assetLoader.imgs.bg, 0, 0);
-
-      // Pan background
-      sky.x -= sky.speed;
-      backdrop.x -= backdrop.speed;
-      backdrop2.x -= backdrop2.speed;
-
-      // draw images side by side to loop
-      ctx.drawImage(assetLoader.imgs.sky, sky.x, sky.y);
-      ctx.drawImage(assetLoader.imgs.sky, sky.x + canvas.width, sky.y);
-
-      ctx.drawImage(assetLoader.imgs.backdrop, backdrop.x, backdrop.y);
-      ctx.drawImage(assetLoader.imgs.backdrop, backdrop.x + canvas.width, backdrop.y);
-
-      ctx.drawImage(assetLoader.imgs.backdrop2, backdrop2.x, backdrop2.y);
-      ctx.drawImage(assetLoader.imgs.backdrop2, backdrop2.x + canvas.width, backdrop2.y);
-
-      // If the image scrolled off the screen, reset
-      if (sky.x + assetLoader.imgs.sky.width <= 0)
-        sky.x = 0;
-      if (backdrop.x + assetLoader.imgs.backdrop.width <= 0)
-        backdrop.x = 0;
-      if (backdrop2.x + assetLoader.imgs.backdrop2.width <= 0)
-        backdrop2.x = 0;
-    };
-
-    /**
-     * Reset background to zero
-     */
-    this.reset = function()  {
-      sky.x = 0;
-      sky.y = 0;
-      sky.speed = 0.2;
-
-      backdrop.x = 0;
-      backdrop.y = 0;
-      backdrop.speed = 0.4;
-
-      backdrop2.x = 0;
-      backdrop2.y = 0;
-      backdrop2.speed = 0.6;
+      index++;
     }
-
-    return {
-      draw: this.draw,
-      reset: this.reset
-    };
-  })();
-
-  /**
-   * Game loop
-   */
-  function animate() {
-    requestAnimFrame( animate );
-
-    background.draw();
-
-    for (i = 0; i < ground.length; i++) {
-      ground[i].x -= player.speed;
-      ctx.drawImage(assetLoader.imgs.grass, ground[i].x, ground[i].y);
-    }
-
-    if (ground[0].x <= -platformWidth) {
-      ground.shift();
-      ground.push({'x': ground[ground.length-1].x + platformWidth, 'y': platformHeight});
-    }
-
-    player.anim.update();
-    player.anim.draw(64, 260);
+    // console.log(str);
   }
+}
 
-  /**
-   * Request Animation Polyfill
-   */
-  var requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            function(callback, element){
-              window.setTimeout(callback, 1000 / 60);
-            };
-  })();
 
-  /**
-   * Start the game - reset all variables and entities, spawn platforms and water.
-   */
-  function startGame() {
-    // setup the player
-    player.width  = 60;
-    player.height = 96;
-    player.speed  = 6;
-    player.sheet  = new SpriteSheet('imgs/sprite_0.png', player.width, player.height);
-    player.anim   = new Animation(player.sheet, 4, 0, 15);
+function drawSprite() {
+  // drawImage( image to draw,
+  //            topCornerX in that image, topCornerY in that image, 
+  //            offsetToBottomX, offsetToBottomY,
+  //            where to put it on the canvasX, where to put it on the canvasY,
+  //            offsetToBottomCanvasX, offsetToBottomCanvasY )
+  // ctx implies the canvas, so image to draw is the sprite sheet (or just
+  //   an image)
+  // The offsets are usually the same, but it will shrink/expand the subimage
+  //   if they are different.
+  // draw blue square
+   ctx.drawImage(SpriteImage, 
+     // What part of the image 
+     SpriteCol*32, SpriteRow*32, SpriteWidth, SpriteHeight, 
+     // Where it goes on the canvas
+     myX, myY, SpriteWidth, SpriteHeight);
+}
 
-    // create the ground tiles
-    for (i = 0, length = Math.floor(canvas.width / platformWidth) + 2; i < length; i++) {
-      ground[i] = {'x': i * platformWidth, 'y': platformHeight};
-    }
 
-    background.reset();
+function Tick() {
 
-    animate();
+  if (moved) {
+    moved = false;
   }
+}
 
-  assetLoader.downloadAll();
-})();
+
+function loadComplete() {
+  console.log("Load is complete."); 
+  canvas = document.getElementById("theCanvas");
+  ctx = canvas.getContext("2d");
+
+  drawTiles();
+  drawSprite();
+  //myInterval = self.setInterval("Tick()", INTERVAL);
+}
+
+
+// Do the new coordinates cause a collision?
+function checkCollision(newX, newY) {
+  // Use floor since result should be integer
+  var tempRow = Math.floor(newY / tileWidth);
+  var tempCol = Math.floor(newX / tileHeight);
+  // So tempRow, tempCol are integer offsets into array.
+  // Is there a non-0 feature there?
+  if (myarray[tempRow * MAXCOL + tempCol] == 0)
+    return true;
+
+  // Did we get to the goal?
+  
+  if (!goal_found && (myarray[tempRow * MAXCOL + tempCol] == GOALTILE)) {
+    green(); 
+
+  }
+  
+  return false;
+}
+
+// What to do when the user presses a key.
+function whenKeyPressed(key) {
+    // Erase the sprite from its current location.
+  eraseSprite();
+  // Remember the old x,y values
+  var newX = myX;
+  var newY = myY;
+
+  switch (key) {
+    case 28:  // Right arrow was pressed 
+      // Go right 
+	  SpriteRow=1;
+	  SpriteCol+=1;
+	  if(SpriteCol>7){
+		SpriteCol=0;
+	  }
+      newX = newX + moveAmount;
+      if (newX + squareWidth > WIDTH)
+        newX = WIDTH - squareWidth;
+      moved = true;
+      break;
+    case 29:  // Left arrow, ASCII 29 
+      // Go left 
+	  SpriteRow=0
+	  SpriteCol-=1;
+	  if(SpriteCol<0){
+		SpriteCol=7;
+	  }
+      newX = newX - moveAmount;
+      if (newX < 0)
+        newX = 0;
+      moved = true;
+      break;
+    case 30:  // Up arrow was pressed 
+      // Go up 
+	  SpriteRow=2
+	  SpriteCol-=1;
+	  if(SpriteCol<0){
+		SpriteCol=7;
+	  }
+      newY = newY - moveAmount;
+      if (newY < 0)
+        newY = 0;
+      moved = true;
+      break;
+    case 31:  // Down arrow was pressed 
+      // Go down 
+	  SpriteRow=3;
+	  SpriteCol+=1;
+	  if(SpriteCol>7){
+		SpriteCol=0;
+	  }
+      newY = newY + moveAmount;
+      if (newY + squareHeight > HEIGHT)
+        newY = HEIGHT - squareWidth;
+      moved = true;
+      break;
+  }
+  // Check the new coords
+  // newX, newY is just the upper-left corner. 
+  // Also check the upper-right, lower-left, and lower-right corners.
+  if ((checkCollision(newX, newY)) && 
+      (checkCollision(newX + SpriteWidth, newY)) &&
+      (checkCollision(newX, newY + SpriteHeight)) && 
+      (checkCollision(newX + SpriteWidth, newY + SpriteHeight))) {
+    myX = newX;
+    myY = newY;
+  } else {
+    console.log("could not move there");
+  }
+  // Show a new image
+  drawSprite();    
+}
